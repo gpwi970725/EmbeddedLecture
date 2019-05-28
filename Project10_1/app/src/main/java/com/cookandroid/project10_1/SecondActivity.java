298db5db6a80623e2450ac284f9aae12b6c3e7e5
gpwi970725 <hengzizng@kangwon.ac.kr>
2019-05-28 10:29:56 +0900

    Complete Project10_1

---
+++ Project10_1/app/src/main/java/com/cookandroid/project10_1/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.cookandroid.project10_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Embedded on 2019-05-28.
+ */
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
